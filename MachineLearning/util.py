9b02fcb8dac2cd4d2e432060c5964810855675e3
Christian Wolff <cgwolff@email.sc.edu>
2024-11-19 22:27:02 -0500

    Initial commit

--- MachineLearning/util.py
+++ MachineLearning/util.py
@@ -1,8 +1,8 @@
 class Node():
-    def __init__(self, state, parent, action):
-        self.state = state
+    def __init__(self, actor, parent, movie):
+        self.state = actor
         self.parent = parent
-        self.action = action
+        self.action = movie
 
 
 class StackFrontier():
